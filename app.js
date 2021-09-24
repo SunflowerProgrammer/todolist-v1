523990354640c515b7accfe7bde65bab86378539
Dennis <sigur@mailbox.org>
2021-09-24 11:36:51 +0200

    add premade stylesheet (serve via express.static, create divs with id's & classes), include ejs layouts: create header & footer section, add a work & about route and serve data according to route

--- app.js
+++ app.js
@@ -6,26 +6,43 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 var items = [];
+var workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
-
+ 
 app.get("/", (req,res)=>{
     var date = new Date();
     var options = {day: "numeric", weekday: "long", month: "long"};
     var currentDate = date.toLocaleDateString("en-US", options);
 
-    res.render("list", {date: currentDate, items: items});
+    res.render("list", {listTitle: currentDate, items: items, route: "/"});
 }); 
 
 app.post("/", (req,res)=>{
-    var item = req.body.item;
-    items.push(item);
+    var newItem = req.body.newItem;
+    items.push(newItem);
 
     res.redirect("/");
+});
+
+
+app.get("/work", (req,res)=>{
+    res.render("list", {listTitle: "Work", items: workItems, route: "/work"});
+});
+
+app.post("/work", (req,res)=>{
+    var newItem = req.body.newItem;
+    workItems.push(newItem);
+
+    res.redirect("/work");
+});
+
+app.get("/about", (req,res)=>{
+    res.render("about");
 });
 
 app.listen(PORT, ()=>{
